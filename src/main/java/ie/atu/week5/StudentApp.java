f3994927b8a352e12ba109d2c059428d78fd4681
ellenmcintyre123 <g003898482@atu.ie>
2022-10-24 10:40:04 +0100

    new methods

--- src/main/java/ie/atu/week5/StudentApp.java
+++ src/main/java/ie/atu/week5/StudentApp.java
@@ -5,18 +5,40 @@ import java.util.Scanner;
 public class StudentApp {
 
     public static void main(String[] args) {
+        //first instance of student
         System.out.println("Please enter your name");
         Scanner input = new Scanner(System.in);
         String name = input.nextLine();
-        System.out.println("The name you entered is: " + name);
+        Student studApp = new Student();
+        studApp.setName(name);
+        System.out.println("The name you entered is: " + studApp.getName());
 
         System.out.println("Please enter your email");
         String email = input.nextLine();
-        System.out.println("The email you entered is: " + email);
+        studApp.setEmail(email);
+        System.out.println("The email you entered is: " + studApp.getEmail());
 
         System.out.println("Please enter your course");
         String course = input.nextLine();
-        System.out.println("The course you entered is: " + course);
+        studApp.setCourse(course);
+        System.out.println("The course you entered is: " + studApp.getCourse());
+
+        //second instance of student
+        Student studApp2 = new Student();
+        System.out.println("Please enter second name");
+        String name2 = input.nextLine();
+        studApp2.setName(name2);
+        System.out.println("The name you entered is: " + studApp2.getName());
+
+        System.out.println("Please enter second email");
+        String email2 = input.nextLine();
+        studApp2.setEmail(email2);
+        System.out.println("The email you entered is: " + studApp2.getEmail());
+
+        System.out.println("Please enter course");
+        String course2 = input.nextLine();
+        studApp2.setCourse(course2);
+        System.out.println("The course you entered is: " + studApp2.getCourse());
 
     }
 }
