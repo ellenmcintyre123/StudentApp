c3ed38fc465643bd47bd591c64f3a4b9eb76fbb7
ellenmcintyre123 <g003898482@atu.ie>
2022-10-24 09:59:48 +0100

    prompt user for course

--- src/main/java/ie/atu/week5/StudentApp.java
+++ src/main/java/ie/atu/week5/StudentApp.java
@@ -7,12 +7,16 @@ public class StudentApp {
     public static void main(String[] args) {
         System.out.println("Please enter your name");
         Scanner input = new Scanner(System.in);
-        String name = input.next();
+        String name = input.nextLine();
         System.out.println("The name you entered is: " + name);
 
         System.out.println("Please enter your email");
-        String email = input.next();
+        String email = input.nextLine();
         System.out.println("The email you entered is: " + email);
+
+        System.out.println("Please enter your course");
+        String course = input.nextLine();
+        System.out.println("The course you entered is: " + course);
 
     }
 }
